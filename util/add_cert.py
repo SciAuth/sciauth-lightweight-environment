e4ca0b269e1b143e20da0252651e5bb0297224dd
Brian Aydemir <baydemir@morgridge.org>
2023-01-31 16:33:56 -0600

    Clean up how certificate bundles are updated

---
+++ util/add_cert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Append a certificate to "trusted" certificate bundles.
+"""
+
+import sys
+import textwrap
+
+
+def main():
+    cert_file = sys.argv[1]
+    system_bundle_file = sys.argv[2]
+
+    with open(cert_file, encoding="utf-8") as fp:
+        cert = textwrap.dedent(
+            """\
+
+            # SciAuth Lightweight Environment Self-Signed Certificate
+            """
+        )
+        cert += fp.read()
+
+    with open(system_bundle_file, mode="a", encoding="utf-8") as fp:
+        print(cert, file=fp)
+
+    try:
+        import certifi
+
+        with open(certifi.where(), mode="a", encoding="utf-8") as fp:
+            print(cert, file=fp)
+    except ImportError:
+        pass
+
+
+if __name__ == "__main__":
+    main()
